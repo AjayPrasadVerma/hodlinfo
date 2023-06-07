807590badde4d1b43c842e8352a04f6bf41422cd
Ajay <imajvermaofficial@gmail.com>
2023-06-07 15:22:59 +0530

    modifying some logic

--- app.js
+++ app.js
@@ -25,33 +25,53 @@ app.get('', async (req, res) => {
         const response = await axios.get('https://api.wazirx.com/api/v2/tickers');
         const tickersData = response.data;
 
+        // const top10Tickers = Object.values(tickersData).slice(0, 10);
+        // cache.set('top10Tickers', top10Tickers, 60);
+
+        // for (const tickerData of top10Tickers) {
+        //     const existingTicker = await Ticker.findOne({ name: tickerData.name }).maxTimeMS(20000);
+
+        //     if (existingTicker) {
+
+        //         existingTicker.last = tickerData.last;
+        //         existingTicker.buy = tickerData.buy;
+        //         existingTicker.sell = tickerData.sell;
+        //         existingTicker.volume = tickerData.volume;
+        //         existingTicker.base_unit = tickerData.base_unit;
+
+        //         await existingTicker.save();
+        //     } else {
+
+        //         await Ticker.create({
+        //             name: tickerData.name,
+        //             last: tickerData.last,
+        //             buy: tickerData.buy,
+        //             sell: tickerData.sell,
+        //             volume: tickerData.volume,
+        //             base_unit: tickerData.base_unit,
+        //         });
+        //     }
+        // }
+
         const top10Tickers = Object.values(tickersData).slice(0, 10);
         cache.set('top10Tickers', top10Tickers, 60);
 
         for (const tickerData of top10Tickers) {
-            const existingTicker = await Ticker.findOne({ name: tickerData.name }).maxTimeMS(20000);
-
-            if (existingTicker) {
-
-                existingTicker.last = tickerData.last;
-                existingTicker.buy = tickerData.buy;
-                existingTicker.sell = tickerData.sell;
-                existingTicker.volume = tickerData.volume;
-                existingTicker.base_unit = tickerData.base_unit;
-
-                await existingTicker.save();
-            } else {
-
-                await Ticker.create({
-                    name: tickerData.name,
-                    last: tickerData.last,
-                    buy: tickerData.buy,
-                    sell: tickerData.sell,
-                    volume: tickerData.volume,
-                    base_unit: tickerData.base_unit,
-                });
-            }
+            await Ticker.updateOne(
+                { name: tickerData.name },
+                {
+                    $set: {
+                        last: tickerData.last,
+                        buy: tickerData.buy,
+                        sell: tickerData.sell,
+                        volume: tickerData.volume,
+                        base_unit: tickerData.base_unit,
+                    },
+                },
+                { upsert: true }
+            );
         }
+
 
         const foundData = await Ticker.find({});
 
